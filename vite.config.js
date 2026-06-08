71d1dbc36345f86e83e0a5b336c1f20df9bf2276
Aarav Harit <aaravharit418@gmail.com>
2026-06-09 02:41:51 +0530

    Project_DDR

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,   // exposes on local network so phones can access it
+    port: 5173,
+  },
 })
